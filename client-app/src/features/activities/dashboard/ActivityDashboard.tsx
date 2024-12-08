7fc6cbf0348bc98d573604ed838be450b544b3ca
amelia9x <kieuanh.jp@gmail.com>
2024-12-09 10:55:53 +1100

    Add react router with routes

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,14 +1,21 @@
-import React from "react";
-import { Grid, List } from "semantic-ui-react";
+import React, { useEffect } from "react";
+import { Grid } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 export default observer (function ActivityDashboard() {
   const { activityStore } = useStore()
   const {selectedActivity, editMode} = activityStore 
+
+  useEffect(() => {
+    activityStore.loadActivities()
+  }, [activityStore])
+
+  if(activityStore.initialLoading) return <LoadingComponent content='Loading app'/>
 
   return (
     <Grid style={{marginRight: '20px'}}>
